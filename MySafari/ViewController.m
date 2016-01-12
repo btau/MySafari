b70a9afa32545daa8eb21740b19c2759c735ae2e
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-01-12 15:09:37 -0600

    created project through step five

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -19,9 +22,24 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSURL *url = [NSURL URLWithString:self.urlTextField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    return true;
 }
+
+-(void)webViewDidStartLoad:(UIWebView *)webView{
+    [self.spinner startAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    [self.spinner stopAnimating];
+}
+
+- (IBAction)onBackButtonPressed:(id)sender {
+    [self.webView goBack];
+}
+
 
 @end
